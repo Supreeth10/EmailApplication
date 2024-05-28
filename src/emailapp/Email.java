0e03521744c49ff883b7e6bb2da3ec2a7c4cf775
supreeth10 <smuddu2@pdx.edu>
2024-05-27 17:43:03 -0700

    added the password method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,10 +1,12 @@
 package emailapp;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Email {
     private String firstName;
     private String lastName;
     private String password;
+    private int defaultPasswordLength = 10;
     private String department;
     private int mailboxCapacity;
     private String alternateEmail;
@@ -13,24 +15,43 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println(this.firstName + " " + this.lastName);
+
+        // call a function to ask for the department and set it
+        this.department = setDepartment();
+
+
+        //call a method that returns random password
+        this.password = randomPassword(this.defaultPasswordLength);
+        System.out.println(this.firstName + " " + this.lastName + " Department " + this.department + " Password " + this.password);
     }
 
     //Ask for the department
-    public void setDepartment(String department) {
-        this.department = department;
+    private String setDepartment() {
+        System.out.println("Enter the department  \n1 for Sales \n2 for Development \n3 for Accounting \n0 for none");
+        Scanner In = new Scanner(System.in);
+        int depChoice = In.nextInt();
+        if (depChoice == 1) {
+            this.department = "sales";
+        }
+        else if (depChoice == 2) {
+            this.department = "dev";
+        }
+        else if (depChoice == 3) {
+            this.department = "acct";
+        }
+        else {return "";}
+        return department;
     }
 
     //Generate a random password
-    public void setPassword(){
+    public String randomPassword(int length){
         final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         Random random = new Random();
-        int length = 10;
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
             sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
         }
-        this.password = sb.toString();
+        return sb.toString();
     }
 
     //Set the mailbox capacity
